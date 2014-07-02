01953bbc693dcfba0b18d6030a9a16a2943d60be
Daniel Seidel <daniel.seidel@dlr.de>
2014-07-02 18:54:34 +0200

    changed script to include all non-hidden files in dotfiles folder

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,23 +8,21 @@
 
 dir=~/.dotfiles                    # dotfiles directory
 olddir=~/.dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim profile zshrc"    # list of files/folders to symlink in homedir
+#files="bashrc vimrc vim profile zshrc"    # list of files/folders to symlink in homedir
+files=[!m]*
 
 ##########
 
 # create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
-echo "...done"
 
 # change to the dotfiles directory
-echo "Changing to the $dir directory"
 cd $dir
-echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
-    echo "Moving any .$file from ~ to $olddir"
+    echo "Moving .$file from ~ to $olddir"
     mv ~/.$file $olddir
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
