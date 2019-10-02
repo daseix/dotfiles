505ef6cd8fc21bb996b13a6ee817867874a5d894
Daniel Seidel <daniel.seidel@dlr.de>
2019-10-02 10:44:42 +0200

    added xrandr script for big monitor split and config files to load keyboard config

---
+++ xrandr_splitconfig.sh
@@ -0,0 +1,10 @@
+#xrandr --setmonitor DP-1-1 1706/399x1440/336+0+0 DP-1 
+#xrandr --setmonitor DP-1-2 1708/399x1440/336+1706+0 none 
+#xrandr --setmonitor DP-1-3 1706/399x1440/336+03414+0 none
+
+#xrandr --fb 1706x1440
+#xrandr --fb 1708x1440
+#xrandr --fb 1706x1440
+
+xrandr --setmonitor DVI-I-1 2560x1440+0+0 DVI-I-1
+xrandr --output DVI-I-1 --primary --mode 2560x1440
