00c93702ae127f9bedfa97f822d8166e50024565
Daniel Seidel <daniel.seidel@dlr.de>
2020-06-25 12:18:50 +0200

    changed set layout script

--- set_layout.sh
+++ set_layout.sh
@@ -1,1 +1,3 @@
-us_layout
+setxkbmap de -variant nodeadkeys 
+#xkbcomp -I/home/seid_da/.dotfiles /home/seid_da/.dotfiles/us_intl_hjkl_numblock.xkb :0
+xkbcomp /home/seid_da/.dotfiles/us_intl_hjkl_numblock.xkb $DISPLAY
