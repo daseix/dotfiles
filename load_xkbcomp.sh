ccda9949ec59126a2ceae851c243fdfdb07be131
Daniel Seidel <daniel.seidel@dlr.de>
2024-07-23 13:19:44 +0200

    update layout script

--- load_xkbcomp.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/sh
-setxkbmap de -variant nodeadkeys
-xkbcomp -I~/.dotfiles ~/.dotfiles/us_intl_hjkl_numblock.xkb $DISPLAY
--- set_layout.sh
+++ set_layout.sh
@@ -1,3 +1,6 @@
-setxkbmap de -variant nodeadkeys 
-#xkbcomp -I/home/seid_da/.dotfiles /home/seid_da/.dotfiles/us_intl_hjkl_numblock.xkb :0
-xkbcomp /home/seid_da/.dotfiles/us_intl_hjkl_numblock.xkb $DISPLAY
+#!/bin/bash
+
+xset r rate 250 60
+sleep 1
+setxkbmap us
+xkbcomp -I/home/seid_da/.dotfiles /home/seid_da/.dotfiles/us_intl_hjkl_numblock.xkb $DISPLAY
