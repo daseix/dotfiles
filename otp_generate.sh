f261f5882c5c46f5f16e8a84e5917eea21d03b4c
Daniel Seidel <daniel.seidel@dlr.de>
2025-01-22 10:08:29 +0100

    simplified otp

--- otp_generate.sh
+++ otp_generate.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-oathtool -b --totp `python3 -c "import keyring; print(keyring.get_credential(\"RMC\", None).password)"`
+oathtool -b --totp `keyring -b keyring.backends.libsecret.Keyring get otp seid_da`
+
+#oathtool -b --totp `python3 -c "import keyring; print(keyring.get_credential(\"RMC\", None).password)"`
 
 # import keyring
 # # keyring.set_password("RMC", "seid_da", secret)
--- ssh_otp.sh
+++
@@ -1,2 +0,0 @@
-#/bin/bash
-./ssh_otp.exp $(./ssh_generate_otp.sh)
